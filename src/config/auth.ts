cbdab2d8681ef134d1d9f4bafc20f6125c7468e3
Gabriel Moura <mourasb.gabriel@gmail.com>
2021-10-10 16:34:03 -0300

    fix: commit auth config file

---
+++ src/config/auth.ts
@@ -0,0 +1,7 @@
+const authConfig = {
+  jwt: {
+    secret: process.env.JWT_SECRET,
+  },
+};
+
+export default authConfig;
--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -1,32 +1,27 @@
 import { Router } from 'express';
 import { parseISO } from 'date-fns';
 import { getCustomRepository } from 'typeorm';
-
 import AppointmentsRepository from '../repositories/AppointmentsRepository';
 import CreateAppointmentService from '../services/CreateAppointmentService';
-
-import ensureAuthentication from '../middlewares/ensureAuthentication';
+// import ensureAuthentication from '../middlewares/ensureAuthentication';
 
 const appointmentsRouter = Router();
 
-appointmentsRouter.use(ensureAuthentication);
+// appointmentsRouter.use(ensureAuthentication);
 
 appointmentsRouter.get('/', async (request, response) => {
   const appointmentsRepository = getCustomRepository(AppointmentsRepository);
   const appointments = await appointmentsRepository.find();
-
   return response.json(appointments);
 });
 
-// eslint-disable-next-line consistent-return
 appointmentsRouter.post('/', async (request, response) => {
   const { provider_id, date } = request.body;
-
   const parsedDate = parseISO(date);
 
-  const createAppointment = new CreateAppointmentService();
+  const createAppointmentService = new CreateAppointmentService();
 
-  const appointment = await createAppointment.execute({
+  const appointment = await createAppointmentService.execute({
     date: parsedDate,
     provider_id,
   });
